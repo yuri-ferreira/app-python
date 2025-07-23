7576d7f324dfb86579351f010eb53af7e220d0f9
Yuri Brigido Ferreira <37347459+yuri-ferreira@users.noreply.github.com>
2025-07-23 10:00:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 app = FastAPI() 
 @app.get("/") 
 async def root(): 
-    return {"message": "Hello World"} 
+    return {"message": "Aoba mundo! Essa é uma nova mensagem para testar se está tudo funcionando! 🥳"} 
